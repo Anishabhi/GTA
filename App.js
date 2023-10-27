0a59a9b04cdb186dee150bbe00f00d11ef2f0ea5
Anishabhi <116378279+Anishabhi@users.noreply.github.com>
2023-10-27 10:21:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import ReactDOM from 'react-dom';
+import FrontPage from './frontpage.js';
+import front from './front.css'
+import React from 'react';
+import './App.css'; // You can create a CSS file for your App component if needed
+
+function App() {
+  return (
+    
+      <FrontPage />
+   
+  );
+}
+
+export default App;
+
+
+
